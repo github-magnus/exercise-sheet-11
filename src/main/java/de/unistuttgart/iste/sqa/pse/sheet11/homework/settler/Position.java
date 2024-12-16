b3cd06381fce3ba8bcc3030c3156d0c5043fed49
Sarah Sophie Stieß <sarah.stiess@iste.uni-stuttgart.de>
2024-12-16 09:32:57 +0100

    add missing documentation and defensive programming.

--- src/main/java/de/unistuttgart/iste/sqa/pse/sheet11/homework/settler/Position.java
+++ src/main/java/de/unistuttgart/iste/sqa/pse/sheet11/homework/settler/Position.java
@@ -3,23 +3,46 @@ package de.unistuttgart.iste.sqa.pse.sheet11.homework.settler;
 /**
  *  Position of a Settler or Resource.
  *
- *  The Position is defined by longitude and latitude.
+ *  The Position is defined by longitude and latitude, both are saved as decimal degrees.
  */
 public final class Position {
-	private final long longitude;
-	private final long latitude;
+	private final double longitude;
+	private final double latitude;
 
-	public Position(final long longitude, final long latitude) {
+	/**
+	 *
+	 * @param longitude longitude of the position, must be in [-180°, +180°]
+	 * @param latitude of the position, must be in [-90°, +90°]
+	 */
+	public Position(final double longitude, final double latitude) {
 		super();
+		if (longitude < -180.0 || longitude > 180) {
+			throw new IllegalArgumentException(
+					"Illegal value for longitude. Value must be in [-180°, +180°] but is not.");
+		}
+		if (latitude < -90.0 || latitude > 90) {
+			throw new IllegalArgumentException("Illegal value for latitude. Value must be in [-90°, +90°] but is not.");
+		}
+
 		this.longitude = longitude;
 		this.latitude = latitude;
 	}
 
-	public long getLongitude() {
+	/**
+	 * Get the longitude of the position in decimal degrees.
+	 *
+	 * @return longitude of the position
+	 */
+	public double getLongitude() {
 		return longitude;
 	}
 
-	public long getLatitude() {
+	/**
+	 * Get the latitude of the position in decimal degrees.
+	 *
+	 * @return latitude of the position.
+	 */
+	public double getLatitude() {
 		return latitude;
 	}
 }
--- src/main/java/de/unistuttgart/iste/sqa/pse/sheet11/homework/settler/ResourceType.java
+++ src/main/java/de/unistuttgart/iste/sqa/pse/sheet11/homework/settler/ResourceType.java
@@ -6,11 +6,24 @@ package de.unistuttgart.iste.sqa.pse.sheet11.homework.settler;
 public final class ResourceType {
 	private final String label;
 
+	/**
+	 * Create a new instance of {@link ResourceType}
+	 *
+	 * @param label label of the type. Must not be {@code null} or blank.
+	 */
 	public ResourceType(final String label) {
 		super();
+		if (label == null || label.isBlank()) {
+			throw new IllegalArgumentException("The resource type's label must be neither null nor blank, but is.");
+		}
 		this.label = label;
 	}
 
+	/**
+	 * Get the label of the resource type.
+	 *
+	 * @return label of the resource type.
+	 */
 	public String getLabel() {
 		return label;
 	}
